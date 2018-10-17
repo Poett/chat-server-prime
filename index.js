8c08ed3dc3dfca78660413036753d84d4161340a
Caleb de Coux <decouxc82@gmail.com>
2018-10-17 14:31:54 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+const chat = io.of("/chat");
+
+
+app.use(express.static("webfiles"));
+
+app.get('/', (req, res) => 
+{
+    res.send('Index');
+});
+
+app.get('/chat', (req, res) => 
+{
+    res.sendFile(__dirname + "/chat.html");
+});
+
+
+io.on('connection', (socket) => 
+{
+    console.log('client connected to index');
+});
+
+chat.on('connection', (socket) =>
+{
+    socket.join('chat');
+
+    chat.clients((error, clients ) => 
+    {
+        if (error) throw error;
+        console.log(clients);
+    });
+
+    console.log("Client " + socket.id + " entered chat");
+
+    socket.on('disconnect', () =>
+    {
+        console.log('user disconnected');
+    });
+
+    socket.on('chat message', (msg) =>
+    {
+        console.log('message: ', msg);
+        chat.to('chat').emit("chat message", msg, socket.id);
+    })
+});
+
+http.listen(8080, "0.0.0.0");
