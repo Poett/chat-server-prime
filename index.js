6a00dba83670952fc810683d8e5b04f0d8303d5c
Caleb DeCoux <decouxc82@gmail.com>
2019-01-15 11:23:32 -0600

    updated styles

--- index.js
+++ index.js
@@ -1,51 +1,67 @@
+//Express HTTP Setup 
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+
+//Socket.IO Setup
 var io = require('socket.io')(http);
-var path = require('path');
-const chat = io.of("/chat");
+const chat = io.of("/chat"); //create namespace '/chat'
+var clientCount = 0;
+let port = process.env.PORT || 8082;
 
 
-app.use(express.static("webfiles"));
+app.use(express.static("webfiles")); //define a subdirectory that clients can request from
 
+//Define HTTP GETs
 app.get('/', (req, res) => 
 {
-    res.send('Index');
+    res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/chat', (req, res) => 
 {
+    console.log(req.username);
     res.sendFile(__dirname + "/chat.html");
 });
 
+//Define and load Socket.IO connection events
 
+//host domain
 io.on('connection', (socket) => 
 {
-    console.log('client connected to index');
+    console.log('client connected');
 });
 
+//chat namespace
 chat.on('connection', (socket) =>
 {
-    socket.join('chat');
+    socket.join('general'); //join room 'general' within '/chat'
+	
+	clientCount = clientCount + 1;
+	socket.clientNumber = clientCount;
 
-    chat.clients((error, clients ) => 
-    {
-        if (error) throw error;
-        console.log(clients);
-    });
+    console.log("Client " + socket.clientNumber + " entered chat");
 
-    console.log("Client " + socket.id + " entered chat");
-
+	//Define disconnect event - reserved event from socket.io library
     socket.on('disconnect', () =>
     {
         console.log('user disconnected');
     });
 
+	//Define a 'chat message' event
     socket.on('chat message', (msg) =>
     {
-        console.log('message: ', msg);
-        chat.to('chat').emit("chat message", msg, socket.id);
+        let label = "Client " + socket.clientNumber + ": ";
+        console.log(label, msg);
+		
+		//broadcast to all clients in '/chat', room 'general'
+        chat.to('general').emit("chat message", msg, label); 
     })
 });
 
-http.listen(process.env.PORT || 8080);
+
+//Begin listening after everything defined
+http.listen(port, () => 
+{
+    console.log(`Listening on port ${port}`);
+});
